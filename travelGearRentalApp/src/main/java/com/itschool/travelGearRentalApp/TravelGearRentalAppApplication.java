5485f733cad0cef96dc4dbd54761c4f4dd3b3a32
raduM <23radumihaiu@gmail.com>
2024-10-09 18:31:28 +0300

    Git setup commit

---
+++ travelGearRentalApp/src/main/java/com/itschool/travelGearRentalApp/TravelGearRentalAppApplication.java
@@ -0,0 +1,13 @@
+package com.itschool.travelGearRentalApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelGearRentalAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelGearRentalAppApplication.class, args);
+	}
+
+}
---
+++ travelGearRentalApp/src/test/java/com/itschool/travelGearRentalApp/TravelGearRentalAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itschool.travelGearRentalApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelGearRentalAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
