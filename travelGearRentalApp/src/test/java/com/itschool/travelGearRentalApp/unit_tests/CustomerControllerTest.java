736aa4ea52654b0243a4e20f1664b55e9549d64a
raduM <23radumihaiu@gmail.com>
2024-10-27 16:52:59 +0200

    Unit test | Customer controller unit test added (passed)

---
+++ travelGearRentalApp/src/test/java/com/itschool/travelGearRentalApp/unit_tests/CustomerControllerTest.java
@@ -0,0 +1,58 @@
+package com.itschool.travelGearRentalApp.unit_tests;
+
+import com.itschool.travelGearRentalApp.controllers.CustomerController;
+import com.itschool.travelGearRentalApp.models.dtos.RequestCustomerDTO;
+import com.itschool.travelGearRentalApp.models.dtos.ResponseCustomerDTO;
+import com.itschool.travelGearRentalApp.services.CustomerService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class CustomerControllerTest {
+
+    @InjectMocks
+    private CustomerController customerController;
+    @Mock
+    private CustomerService customerService;
+
+    @Test
+    void testCreateCustomer() {
+        //given
+        RequestCustomerDTO requestCustomerDTO = new RequestCustomerDTO();
+        requestCustomerDTO.setId(1L);
+        requestCustomerDTO.setFirstName("test");
+        requestCustomerDTO.setLastName("test");
+        requestCustomerDTO.setEmail("test@email.com");
+        UUID uuid = UUID.randomUUID();
+        requestCustomerDTO.setCustomerCode(uuid);
+        System.out.println("test uuid is:" + uuid);
+        requestCustomerDTO.setCustomerGender("M");
+
+        ResponseCustomerDTO expectedCustomerDTO = new ResponseCustomerDTO();
+        expectedCustomerDTO.setId(1L);
+        expectedCustomerDTO.setFirstName("test");
+        expectedCustomerDTO.setLastName("test");
+        expectedCustomerDTO.setEmail("test@email.com");
+        expectedCustomerDTO.setCustomerCode(uuid);
+        System.out.println("test uuid is:" + uuid);
+        expectedCustomerDTO.setCustomerGender("M");
+
+        when(customerService.createCustomer(requestCustomerDTO)).thenReturn(expectedCustomerDTO);
+        //when
+        ResponseEntity<ResponseCustomerDTO> response = customerController.createCustomer(requestCustomerDTO);
+
+        //then
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(expectedCustomerDTO, response.getBody());
+    }
+}
