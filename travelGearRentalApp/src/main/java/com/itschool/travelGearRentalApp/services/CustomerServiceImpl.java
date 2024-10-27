3fe9a8be45ae09834f9f2112aee192a2f30f435e
raduM <23radumihaiu@gmail.com>
2024-10-27 12:35:13 +0200

    Test | Unit test for createCustomer method - update

--- travelGearRentalApp/src/main/java/com/itschool/travelGearRentalApp/services/CustomerServiceImpl.java
+++ travelGearRentalApp/src/main/java/com/itschool/travelGearRentalApp/services/CustomerServiceImpl.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
-import java.util.UUID;
 
 @Slf4j
 @Service
--- travelGearRentalApp/src/test/java/com/itschool/travelGearRentalApp/unit_tests/CustomerServiceImplTest.java
+++ travelGearRentalApp/src/test/java/com/itschool/travelGearRentalApp/unit_tests/CustomerServiceImplTest.java
@@ -1,11 +1,14 @@
 package com.itschool.travelGearRentalApp.unit_tests;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.itschool.travelGearRentalApp.exceptions.CustomerEmailAlreadyExistsException;
 import com.itschool.travelGearRentalApp.models.dtos.RequestCustomerDTO;
 import com.itschool.travelGearRentalApp.models.dtos.ResponseCustomerDTO;
 import com.itschool.travelGearRentalApp.models.entities.Customer;
 import com.itschool.travelGearRentalApp.repositories.CustomerRepository;
+import com.itschool.travelGearRentalApp.services.CustomerService;
 import com.itschool.travelGearRentalApp.services.CustomerServiceImpl;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -13,6 +16,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -22,13 +26,31 @@ class CustomerServiceImplTest {
     private ObjectMapper objectMapper;
     @Mock
     private CustomerRepository customerRepository;
-
+    @Mock
+    private RequestCustomerDTO requestCustomerDTO;
     @InjectMocks
     private CustomerServiceImpl customerService;
 
+
+    @Test
+    void testValidateCustomer(){
+        //given
+        RequestCustomerDTO requestDTO = new RequestCustomerDTO();
+        requestDTO.setEmail("test@email.com");
+
+        Customer savedCustomerEmail = new Customer();
+        savedCustomerEmail.setEmail("test@email.com");
+        //when
+        when(customerRepository.findByEmail(requestDTO.getEmail())).thenReturn(requestDTO);
+        //then
+        Assertions.assertEquals(customerRepository, savedCustomerEmail);
+    }
+
+
+
     @Test
     void testCreateCustomer() {
-        //given
+            //given
         RequestCustomerDTO requestCustomerDTO = new RequestCustomerDTO();
         requestCustomerDTO.setFirstName("testFirstName");
         requestCustomerDTO.setLastName("testLastName");
@@ -42,12 +64,14 @@ class CustomerServiceImplTest {
         responseCustomerDTO.setCustomerGender("test");
 
         Customer customerEntity = new Customer();
+        customerEntity.setId(1L);
         customerEntity.setFirstName("testFirstName");
         customerEntity.setLastName("testLastName");
         customerEntity.setEmail("test@email.com");
         customerEntity.setCustomerGender("test");
 
         Customer savedCustomerResponse = new Customer();
+        savedCustomerResponse.setId(1L);
         savedCustomerResponse.setFirstName("testFirstName");
         savedCustomerResponse.setLastName("testLastName");
         savedCustomerResponse.setEmail("test@email.com");
@@ -56,11 +80,9 @@ class CustomerServiceImplTest {
         when(objectMapper.convertValue(requestCustomerDTO, Customer.class)).thenReturn(customerEntity);
         when(customerRepository.save(customerEntity)).thenReturn((customerEntity));
         when(objectMapper.convertValue(savedCustomerResponse,ResponseCustomerDTO.class)).thenReturn(responseCustomerDTO);
-
-        //when
+            //when
         ResponseCustomerDTO savedCustomerDTO = customerService.createCustomer(requestCustomerDTO);
-
-        //then
+            //then
         assertEquals(requestCustomerDTO.getFirstName(), savedCustomerDTO.getFirstName());
     }
 }
